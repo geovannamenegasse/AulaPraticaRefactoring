e3e82e8d4665f6fcc09f0cdb2e1866d9c83a67b5
Geovanna Menegasse <48860848+geovannamenegasse@users.noreply.github.com>
2021-08-07 13:50:53 -0300

    Commit 18

--- Statement.java
+++ Statement.java
@@ -1,3 +1,20 @@
-public class Statement {
+import java.util.Enumeration;
 
+public abstract class Statement {
+    protected abstract String getCustomer(String name);
+    protected abstract String getFigures(Rental each);
+    protected abstract String getResult(Customer aCustomer);
+
+    public String value(Customer aCustomer) {
+        Enumeration rentals = aCustomer.getRentals();
+        String result = getCustomer(aCustomer.getName());
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            //show figures for this rental
+            result += getFigures(each);
+        }
+        //add footer lines
+        result += getResult(aCustomer);
+        return result;
+    }
 }
--- TextStatement.java
+++ TextStatement.java
@@ -1,34 +1,20 @@
-import java.util.Enumeration;
-
 public class TextStatement extends Statement {
 
-    public String value(Customer aCustomer) {
-      Enumeration rentals = aCustomer.getRentals();
-      String result = getCustomer(aCustomer.getName());
-      while (rentals.hasMoreElements()) {
-         Rental each = (Rental) rentals.nextElement();
-         //show figures for this rental
-         result += getFigures(each);
-      }
-      //add footer lines
-      result += getResult(aCustomer);
-      return result;
-   }
-
-   private String getCustomer(String name){
-        return "Rental Record for " + name + "\n";
-   }
-
-    private String getFigures(Rental each){
-        return "\t" + each.getMovie().getTitle()+ "\t" +
-            String.valueOf(each.getCharge()) + "\n";
+    public String getCustomer(String name){
+         return "Rental Record for " + name + "\n";
     }
-
-    private String getResult(Customer aCustomer){
-        return "Amount owed is " +
-        String.valueOf(aCustomer.getTotalCharge()) + "\n" +
-        "You earned " + 
-        String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-        " frequent renter points";
-    }    
-}
+ 
+     public String getFigures(Rental each){
+         return "\t" + each.getMovie().getTitle()+ "\t" +
+             String.valueOf(each.getCharge()) + "\n";
+     }
+ 
+     public String getResult(Customer aCustomer){
+         return "Amount owed is " +
+         String.valueOf(aCustomer.getTotalCharge()) + "\n" +
+         "You earned " + 
+         String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
+         " frequent renter points";
+     }    
+ }
+ 
